a940cc2c839145b3d9fc3ec0d9f0b4165d71bc1e
somatonic <philipp@urlich.ch>
2012-12-02 20:20:11 +0100

    added key support for browsing results with arrow down and up. added escape key to close results. added close results on click outside

--- AjaxSearch.js
+++ AjaxSearch.js
@@ -14,7 +14,30 @@ $(function(){
 	var req = null;
 
 	// build dom for ajax search results
-	$res = $("<div id='ajaxSearch'><div class='ajaxSearch_body'></div></div>");
+	// event listener on "keydown" to be able to prevent the default scrolling in browsers
+	$res = $("<div id='ajaxSearch'><div class='ajaxSearch_body'></div></div>").bind("keydown",function(e){
+			// escape key close results
+			if(e.which == 27) {
+				e.preventDefault();
+				$(this).fadeOut();
+			}
+			// arrow down key
+			if(e.which == 40) {
+				e.preventDefault();
+				var $links = $('#ajaxSearch a');
+				var $focused = $('#ajaxSearch a:focus');
+				var $next = $links.get($links.index($focused)+1);
+				if($next) $next.focus();
+			}
+			// arrow up key
+			if(e.which == 38) {
+				e.preventDefault();
+				var $links = $('#ajaxSearch a');
+				var $focused = $('#ajaxSearch a:focus');
+				var $prev = $links.get($links.index($focused)-1);
+				if($prev) $prev.focus();
+			}
+		});
 	$closebtn = $("<a class='ajaxSearch_close' href='#'>"+txt_close+"</a>");
 	$res.prepend($closebtn);
 	$res.css({'display':'none'});
@@ -22,20 +45,31 @@ $(function(){
 	$res.appendTo($(search_form));
 
 	if($(search_input).length != null){
-		
-		$(search_input).bind("keyup",function () {
-			
+
+		$(search_input).bind("keyup", function(e) {
+
+			// escape key close results
+			if(e.which == 27) {
+				$('#ajaxSearch').fadeOut();
+				return;
+			}
+			// arrow down key go to first link
+			if(e.which == 40) {
+				$('#ajaxSearch a:first').focus();
+				return;
+			}
+
 			var $this = this;
 			var value = $(this).val();
-			
+
 			// if a request running we abort it
 			if (req != null) req.abort();
 
 			if (value.length >= minlength ) {
-				
+
 				// add class to searchfield for loader animation
 				$(search_input).addClass("ajaxSearch_loader");
-				
+
 				req = $.ajax({
 					type: "GET",
 					url: search_url.length > 0 ? search_url : $(this).closest("form").attr("action"),
@@ -63,4 +97,8 @@ $(function(){
 			});
 	}
 
+	// close popup on click outside
+	$(document).click(function(e){
+		if($(e.target).closest('#ajaxSearch').length == 0) $("#ajaxSearch").fadeOut();
+	});
 });
